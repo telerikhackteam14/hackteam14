11b798e310e4978d625f2b1b55ce90bd01f4dc9b
Мартин Гебов <martin.gebov@fdiba.tu-sofia.bg>
2013-11-08 02:13:43 -0600

    Changes made by Мартин Гебов (martin.gebov@fdiba.tu-sofia.bg).

--- Test/scripts/tp.js
+++ Test/scripts/tp.js
@@ -3,6 +3,7 @@ var TeamPulse = function() {
     this.accessToken = null;
     this.projectID = 1;
 	this.storyID = 2;
+    this.currentUser = null;
 }
 
 TeamPulse.prototype = {
@@ -55,10 +56,13 @@ TeamPulse.prototype = {
     },
         
     getCurrentUser: function(onSuccess, onError) {
+        var that = this;
         $.ajax({
             url: this.baseUrl + '/api/users/me',
             type: "GET",
-            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + this.accessToken); },
+            beforeSend: function (xhr) { 
+                xhr.setRequestHeader('Authorization', 'WRAP access_token=' + that.accessToken); 
+            },
             success: function (data) {
                 if(typeof onSuccess === 'function') {
             		onSuccess(data);
@@ -73,10 +77,13 @@ TeamPulse.prototype = {
     },
     
     getUserById: function(userId, onSuccess, onError) {
+        var that = this;
         $.ajax({
             url: this.baseUrl + '/api/users/' + userId,
             type: "GET",
-            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + this.accessToken); },
+            beforeSend: function (xhr) { 
+                xhr.setRequestHeader('Authorization', 'WRAP access_token=' + that.accessToken); 
+            },
             success: function (data) {
                 if(typeof onSuccess === 'function') {
             		onSuccess(data);
@@ -91,10 +98,11 @@ TeamPulse.prototype = {
     },
         
     getItemById: function(id, onSuccess, onError) {
+        var that = this;
         $.ajax({
             url: this.baseUrl + '/api/workitems/'+ id,
             type: "GET",
-            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + this.accessToken); },
+            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + that.accessToken); },
             success: function (data) {
                 if(typeof onSuccess === 'function') {
             		onSuccess(data);
@@ -109,6 +117,7 @@ TeamPulse.prototype = {
     },
 
     getTasksForUser: function(userID, status, onSuccess, onError) {
+        var that = this;
         var oDataFilter = "$filter=AssignedToID eq " + userID;
         if(status) {
         	oDataFilter += " and Status eq '" + status + "'";
@@ -118,7 +127,7 @@ TeamPulse.prototype = {
             url: this.baseUrl + '/api/workitems' + oDataFilter,
             data: null,
             type: "GET",
-            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + this.accessToken); },
+            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + that.accessToken); },
             success: function (data) {
                 if(typeof onSuccess === 'function') {
             		onSuccess(data);
@@ -139,7 +148,7 @@ TeamPulse.prototype = {
             'ParentID': this.storyID,
             'tp_Assigndate_cf': AssignDate
         };
-    
+        var that = this;
         $.ajax({
             url: this.baseUrl + '/api/workitems',
             data: JSON.stringify({
@@ -149,7 +158,9 @@ TeamPulse.prototype = {
             }),
             contentType: 'application/json; charset=utf-8',
             type: "POST",
-            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + this.accessToken); },
+            beforeSend: function (xhr) { 
+                xhr.setRequestHeader('Authorization', 'WRAP access_token=' + that.accessToken); 
+            },
             success: function (data) {
                 if(typeof onSuccess === 'function') {
             		onSuccess(data);
@@ -164,6 +175,7 @@ TeamPulse.prototype = {
     },
 
     updateItem: function(taskId, status, onSuccess, onError) {
+        var that = this;
         $.ajax({
             url: this.baseUrl + '/api/workitems/' + taskId,
             data: JSON.stringify({
@@ -171,7 +183,9 @@ TeamPulse.prototype = {
             }),
             contentType: 'application/json; charset=utf-8',
             type: "PUT",
-            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'WRAP access_token=' + this.accessToken); },
+            beforeSend: function (xhr) { 
+                xhr.setRequestHeader('Authorization', 'WRAP access_token=' + that.accessToken); 
+            },
             success: function (data) {
                 if(typeof onSuccess === 'function') {
             		onSuccess(data);
@@ -188,15 +202,22 @@ TeamPulse.prototype = {
 
 var teamPulse = new TeamPulse();
 teamPulse.login("test", "testtest", function() {
-        teamPulse.getAllUsers(function(data) {
+        teamPulse.getCurrentUser(function(data) {
+            teamPulse.currentUser = data; 
+            teamPulse.getAllUsers(function(data) {
             var dataSource = [];
-            for(var i = 0; i < data.results.length; i++) {
-                dataSource.push({"DisplayName": data.results.displayname});
-                $('#usersDropDownList').append(new Option(data.results[i].displayName, data.results[i].id, true, true));
+            var users = data.results;
+            for(var i = 0; i < users.length; i++) {
+                dataSource.push({"DisplayName": users[i].displayname});
+                if(teamPulse.currentUser.id == users[i].id) {
+                    $('#usersDropDownList').append(new Option(data.results[i].displayName, data.results[i].id, false, true));    
+                } else {
+                    $('#usersDropDownList').append(new Option(data.results[i].displayName, data.results[i].id, false, false));    
+                }
+                
             }
-            
         });
+        });
+        
 });
-
-var teamPulse = new TeamPulse();
 
